ffd9b3bc3a0fe0547fed541e5d1c23e06388d30e
Dominic Hofer <6570912+dominichofer@users.noreply.github.com>
2023-12-20 02:52:51 +0100

    Extends file transfer

--- file_transfer.bat
+++ file_transfer.bat
@@ -1,3 +1,4 @@
+scp jukebox@jukepi:~/rpi-jukebox/log.txt .
 scp *.py jukebox@jukepi:~/rpi-jukebox
 scp *.mp3 jukebox@jukepi:~/rpi-jukebox
 scp rpi_jukebox\*.py jukebox@jukepi:~/rpi-jukebox/rpi_jukebox
